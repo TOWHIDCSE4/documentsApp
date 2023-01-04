58f9a61c70573cd7bb8e22346f58abfb18018c65
Majeed <rahmanmajeed46@gmail.com>
2023-01-04 10:08:29 +0600

    [fix] row click, draft count.

--- src/components/Admin/Application/ApplicationForm.tsx
+++ src/components/Admin/Application/ApplicationForm.tsx
@@ -1,114 +1,193 @@
-import { AlignCenterOutlined, ArrowRightOutlined } from '@ant-design/icons';
-import useBaseHooks from '@root/src/hooks/BaseHook';
-import { Button, Card, Col, Row, Space, Statistic, Input, Table, PaginationProps, Pagination } from 'antd';
-import Meta from 'antd/lib/card/Meta';
-import React from 'react';
-import useStatusCount from '@root/src/hooks/StatusCount';
+import { AlignCenterOutlined, ArrowRightOutlined } from "@ant-design/icons";
+import useBaseHooks from "@root/src/hooks/BaseHook";
+import {
+  Button,
+  Card,
+  Col,
+  Row,
+  Space,
+  Statistic,
+  Input,
+  Table,
+  PaginationProps,
+  Pagination,
+} from "antd";
+import Meta from "antd/lib/card/Meta";
+import React from "react";
+import useStatusCount from "@root/src/hooks/StatusCount";
 
 const { Search } = Input;
 
 const onSearch = (value: string) => console.log(value);
 
-
 const ApplicationForm = () => {
-    const {t, redirect, notify} = useBaseHooks();
-    const status = useStatusCount();
-    
-    return <>
-        <Row gutter={16}>
-            <Col span={12}>
-                <Card type="inner" title="Form List" onClick={() => redirect("frontend.admin.documentTemplate")} extra={<a>{t('pages:application.applicationForm.view')}</a>}>
-                    <Row>
-                        <Col span={6}>
-                            <Statistic title={t('pages:application.applicationForm.submitted')} value={status.total} />
-                        </Col>
-                        <Col span={6}>
-                            <Statistic title={t('pages:application.applicationForm.approve')} value={status["Approve"]} />
-                        </Col>
-                        <Col span={6}>
-                            <Statistic title={t('pages:application.applicationForm.inReview')} value={status["To Be Reviewed"]} />
-                        </Col>
-                        <Col span={6}>
-                            <Statistic title={t('pages:application.applicationForm.rejected')} value={status["Rejected"]} />
-                        </Col>
-                    </Row>
-                </Card>
-            </Col>
-            <Col span={12}>
-                <Card type="inner" title="Form Draft" extra={<a>{t('pages:application.applicationForm.view')}</a>}>
-                    <Statistic title={t('pages:application.applicationForm.draft')} value={status["null"]} />
-                </Card>
-            </Col>
-        </Row>
-        <br />
-        <Card type="inner">
-            <Space>
-                <Button>
-                    <AlignCenterOutlined /> More Filter
-                </Button>
-            </Space>
-            <Search placeholder="Search" className="btn-right" onSearch={onSearch} style={{ width: 300}} />
-        </Card>
-        <Card>
-            <Row justify='space-between'>
-                <Card
-                    hoverable
-                    style={{ width: 240 }}
-                    cover={<img alt="anh-nen" src="https://img.freepik.com/free-vector/vibrant-summer-ombre-background-vector_53876-105765.jpg?w=2000" />}
-                >
-                    <Meta title="Staff Insurance 2022" description="Staff Insurance 2022 is dedicated to tenants & worker." />
-                    <br/>
-                    <Button type="primary" onClick={() => redirect("frontend.admin.application.create")}>
-                        {t('pages:application.applicationForm.apply')} <ArrowRightOutlined />
-                    </Button>
-                </Card>
+  const { t, redirect, notify } = useBaseHooks();
+  const status = useStatusCount();
 
-                <Card
-                    hoverable
-                    style={{ width: 240 }}
-                    cover={<img alt="anh-nen" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU9R3q8guKFAt-fsNfc_7AN6a_o9tQxsAGXnbPDNbuvndQfusFQRHl3wCLAzGRIL0Pr_M&usqp=CAU" />}
-                >
-                    <Meta title="2022 Water Rebate" description="How do you create compelling presentations that wow." />
-                    <br/>
-                    <Button type="primary">
-                        {t('pages:application.applicationForm.apply')} <ArrowRightOutlined />
-                    </Button>
-                </Card>
-
-                <Card
-                    hoverable
-                    style={{ width: 240 }}
-                    cover={<img alt="anh-nen" src="https://img.freepik.com/free-photo/vivid-blurred-colorful-background_58702-2545.jpg?w=2000" />}
-                >
-                    <Meta title="2022 Electric Rebate" description="How do you create compelling presentations that wow." />
-                    <br/>
-                    <Button type="primary">
-                        {t('pages:application.applicationForm.apply')} <ArrowRightOutlined />
-                    </Button>
-                </Card>
-
-                <Card
-                    hoverable
-                    style={{ width: 240 }}
-                    cover={<img alt="anh-nen" src="https://img.freepik.com/free-vector/vibrant-summer-ombre-background-vector_53876-105765.jpg?w=2000" />}
-                >
-                    <Meta title="Staff Insurance 2022" description="Staff Insurance 2022 is dedicated to tenants & worker." />
-                    <br/>
-                    <Button type="primary" onClick={() => redirect("frontend.admin.application.create")}>
-                        {t('pages:application.applicationForm.apply')} <ArrowRightOutlined />
-                    </Button>
-                </Card>
+  return (
+    <>
+      <Row gutter={16}>
+        <Col span={12}>
+          <Card
+            type="inner"
+            title="Form List"
+            onClick={() => redirect("frontend.admin.documentTemplate")}
+            extra={<a>{t("pages:application.applicationForm.view")}</a>}
+          >
+            <Row>
+              <Col span={6}>
+                <Statistic
+                  title={t("pages:application.applicationForm.submitted")}
+                  value={status.total}
+                />
+              </Col>
+              <Col span={6}>
+                <Statistic
+                  title={t("pages:application.applicationForm.approve")}
+                  value={status["Approve"]}
+                />
+              </Col>
+              <Col span={6}>
+                <Statistic
+                  title={t("pages:application.applicationForm.inReview")}
+                  value={status["To Be Reviewed"]}
+                />
+              </Col>
+              <Col span={6}>
+                <Statistic
+                  title={t("pages:application.applicationForm.rejected")}
+                  value={status["Rejected"]}
+                />
+              </Col>
             </Row>
-        </Card>
-        <br/>
-        <div className='text-center'>
-            <Pagination
-                total={85}
-                defaultPageSize={20}
-                defaultCurrent={1}
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card
+            type="inner"
+            title="Form Draft"
+            extra={<a>{t("pages:application.applicationForm.view")}</a>}
+          >
+            <Statistic
+              title={t("pages:application.applicationForm.draft")}
+              value={status["Draft"]}
             />
-        </div>
-    </>
-}
+          </Card>
+        </Col>
+      </Row>
+      <br />
+      <Card type="inner">
+        <Space>
+          <Button>
+            <AlignCenterOutlined /> More Filter
+          </Button>
+        </Space>
+        <Search
+          placeholder="Search"
+          className="btn-right"
+          onSearch={onSearch}
+          style={{ width: 300 }}
+        />
+      </Card>
+      <Card>
+        <Row justify="space-between">
+          <Card
+            hoverable
+            style={{ width: 240 }}
+            cover={
+              <img
+                alt="anh-nen"
+                src="https://img.freepik.com/free-vector/vibrant-summer-ombre-background-vector_53876-105765.jpg?w=2000"
+              />
+            }
+          >
+            <Meta
+              title="Staff Insurance 2022"
+              description="Staff Insurance 2022 is dedicated to tenants & worker."
+            />
+            <br />
+            <Button
+              type="primary"
+              onClick={() => redirect("frontend.admin.application.create")}
+            >
+              {t("pages:application.applicationForm.apply")}{" "}
+              <ArrowRightOutlined />
+            </Button>
+          </Card>
 
-export default ApplicationForm
+          <Card
+            hoverable
+            style={{ width: 240 }}
+            cover={
+              <img
+                alt="anh-nen"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU9R3q8guKFAt-fsNfc_7AN6a_o9tQxsAGXnbPDNbuvndQfusFQRHl3wCLAzGRIL0Pr_M&usqp=CAU"
+              />
+            }
+          >
+            <Meta
+              title="2022 Water Rebate"
+              description="How do you create compelling presentations that wow."
+            />
+            <br />
+            <Button type="primary">
+              {t("pages:application.applicationForm.apply")}{" "}
+              <ArrowRightOutlined />
+            </Button>
+          </Card>
+
+          <Card
+            hoverable
+            style={{ width: 240 }}
+            cover={
+              <img
+                alt="anh-nen"
+                src="https://img.freepik.com/free-photo/vivid-blurred-colorful-background_58702-2545.jpg?w=2000"
+              />
+            }
+          >
+            <Meta
+              title="2022 Electric Rebate"
+              description="How do you create compelling presentations that wow."
+            />
+            <br />
+            <Button type="primary">
+              {t("pages:application.applicationForm.apply")}{" "}
+              <ArrowRightOutlined />
+            </Button>
+          </Card>
+
+          <Card
+            hoverable
+            style={{ width: 240 }}
+            cover={
+              <img
+                alt="anh-nen"
+                src="https://img.freepik.com/free-vector/vibrant-summer-ombre-background-vector_53876-105765.jpg?w=2000"
+              />
+            }
+          >
+            <Meta
+              title="Staff Insurance 2022"
+              description="Staff Insurance 2022 is dedicated to tenants & worker."
+            />
+            <br />
+            <Button
+              type="primary"
+              onClick={() => redirect("frontend.admin.application.create")}
+            >
+              {t("pages:application.applicationForm.apply")}{" "}
+              <ArrowRightOutlined />
+            </Button>
+          </Card>
+        </Row>
+      </Card>
+      <br />
+      <div className="text-center">
+        <Pagination total={85} defaultPageSize={20} defaultCurrent={1} />
+      </div>
+    </>
+  );
+};
+
+export default ApplicationForm;
--- src/pages/admin/documentTemplate/index.tsx
+++ src/pages/admin/documentTemplate/index.tsx
@@ -17,297 +17,279 @@ import auth from "@src/helpers/auth";
 import React, { useState, useRef } from "react";
 import documentService from "@root/src/services/documentService";
 import constantConfig from "@config/constant";
-import { checkStatusByName, checkStatusColor, getCountByStatus } from "@root/src/helpers/utils";
+import {
+  checkStatusByName,
+  checkStatusColor,
+  getCountByStatus,
+} from "@root/src/helpers/utils";
 const { documentStatus } = constantConfig;
 
 interface jsPDFWithPlugin extends jsPDF {
-	autoTable: (options: UserOptions) => jsPDF;
+  autoTable: (options: UserOptions) => jsPDF;
 }
 
 const Index = () => {
-	const { redirect, t, notify } = useBaseHook();
-	const { Title } = Typography;
-	const { Search } = Input;
-	const ButtonGroup = Button.Group;
-	const tableRef = useRef(null);
-	const [selectedIds, setSelectedIds] = useState([]);
-	const [documentTemplates, setDocumentTemplates] = useState(null);
-	const [document, setDocuments] = useState(null);
-	const [statusCount, setStatusCount] = useState(null);
-	const [hiddenDeleteBtn, setHiddenDeleteBtn] = useState(true);
+  const { redirect, t, notify } = useBaseHook();
+  const { Title } = Typography;
+  const { Search } = Input;
+  const ButtonGroup = Button.Group;
+  const tableRef = useRef(null);
+  const [selectedIds, setSelectedIds] = useState([]);
+  const [documentTemplates, setDocumentTemplates] = useState(null);
+  const [document, setDocuments] = useState(null);
+  const [statusCount, setStatusCount] = useState(null);
+  const [hiddenDeleteBtn, setHiddenDeleteBtn] = useState(true);
 
-	const onSearch = (value: string) => console.log(value);
+  const onSearch = (value: string) => console.log(value);
 
-	const fetchData = async (values: any) => {
-		if (!values.sorting.length) {
-			values.sorting = [{ field: "documents.id", direction: "desc" }];
-		}
+  const fetchData = async (values: any) => {
+    if (!values.sorting.length) {
+      values.sorting = [{ field: "documents.id", direction: "desc" }];
+    }
 
-		let [error, documents]: [any, any] = await to(
-			documentService().withAuth().index(values)
-		);
+    let [error, documents]: [any, any] = await to(
+      documentService().withAuth().index(values)
+    );
 
-		if (error) {
-			const { code, message } = error;
-			notify(t(`errors:${code}`), t(message), "error");
-			return {};
-		}
+    if (error) {
+      const { code, message } = error;
+      notify(t(`errors:${code}`), t(message), "error");
+      return {};
+    }
 
-		if (documents) {
-			const resultObj = JSON.parse(JSON.stringify(documents));
-			const result = _.countBy(resultObj.data, "status");
-			setDocuments(resultObj);
-		}
-		const countbystatus =  await getCountByStatus(documents?.data);
-		// const len = Object.keys(countbystatus).filter(item =>item != null)
-		Object.keys(countbystatus).map((item) => {
-			if(documentStatus.hasOwnProperty(item)){
-				// re-name object keys
-				countbystatus[documentStatus[item]] = countbystatus[item];
-				// delete previous key
-				delete countbystatus[item];
+    if (documents) {
+      const resultObj = JSON.parse(JSON.stringify(documents));
+      const result = _.countBy(resultObj.data, "status");
+      setDocuments(resultObj);
+    }
+    const countbystatus = await getCountByStatus(documents?.data);
+    // const len = Object.keys(countbystatus).filter(item =>item != null)
+    Object.keys(countbystatus).map((item) => {
+      if (documentStatus.hasOwnProperty(item)) {
+        // re-name object keys
+        countbystatus[documentStatus[item]] = countbystatus[item];
+        // delete previous key
+        delete countbystatus[item];
+      }
+    });
 
-			}
-			
-			
-		});
+    setStatusCount(countbystatus);
+    console.log(documents);
 
-		setStatusCount(countbystatus);
-		console.log(documents);
-		
-		return documents;
-	};
+    return documents;
+  };
 
-	const onChangeSelection = (data: any) => {
-		if (data.length > 0) setHiddenDeleteBtn(false);
-		else setHiddenDeleteBtn(true);
-		setSelectedIds(data);
-	};
+  const onChangeSelection = (data: any) => {
+    if (data.length > 0) setHiddenDeleteBtn(false);
+    else setHiddenDeleteBtn(true);
+    setSelectedIds(data);
+  };
 
-	const rowSelection = {
-		getCheckboxProps: (record) => ({
-			disabled: record.id == auth().user.id,
-			id: record.id,
-		}),
-	};
+  const rowSelection = {
+    getCheckboxProps: (record) => ({
+      disabled: record.id == auth().user.id,
+      id: record.id,
+    }),
+  };
 
-	const generatePdf = (rowInfo: any) => {
-		return redirect("frontend.admin.application.documentpdf", { id: rowInfo.id });
-	};
+  const generatePdf = (rowInfo: any) => {
+    return redirect("frontend.admin.application.documentpdf", {
+      id: rowInfo.id,
+    });
+  };
 
-	const columns = [
-		{
-			title: t("pages:documents.table.formName"),
-			dataIndex: "formName",
-			key: "documents.formName",
-			sorter: true,
-			filterable: true,
-			render: (text, record) => {
-				return <strong>{text}</strong>;
-			},
-		},
-		{
-			title: t("pages:documentsTemplate.table.formId"),
-			dataIndex: "formId",
-			key: "documents.formId",
-			sorter: true,
-			filterable: true,
-		},
-		{
-			title: t("pages:documentsTemplate.table.issuedBy"),
-			dataIndex: "issuedBy",
-			key: "documents.issuedBy",
-			sorter: true,
-			filterable: true,
-		},
-		{
-			title: t("pages:documentsTemplate.table.issuedDate"),
-			dataIndex: "issuedDate",
-			key: "documents.issuedDate",
-			sorter: true,
-			filterable: true,
-			render: (text: string, record: any) =>
-				text ? moment(text).format("LL") : "",
-			renderFilter: ({ column, confirm, ref }: FilterParam) => (
-				<FilterDatePicker column={column} confirm={confirm} ref={ref} />
-			),
-		},
-		{
-			title: t("pages:documentsTemplate.table.submitter"),
-			dataIndex: "submitter",
-			key: "documents.submitter",
-			sorter: true,
-			filterable: true,
-		},
-		{
-			title: t("pages:documentsTemplate.table.submitDate"),
-			dataIndex: "submitDate",
-			key: "documents.submitDate",
-			sorter: true,
-			filterable: true,
-			render: (text: string, record: any) =>
-				text ? moment(text).format("LL") : "",
-			renderFilter: ({ column, confirm, ref }: FilterParam) => (
-				<FilterDatePicker column={column} confirm={confirm} ref={ref} />
-			),
-		},
-		{
-			title: t("pages:documentsTemplate.table.status"),
-			dataIndex: "status",
-			key: "documents.status",
-			sorter: true,
-			filterable: true,
-			render: (text, record) => {
-				return (
-					<div style={checkStatusColor(text)}>
-						{checkStatusByName(text)}
-					</div>
-				);
-			},
-		},
-		{
-			title: t("pages:documents.table.updatedDate"),
-			dataIndex: "updatedDate",
-			key: "documents.updatedDate",
-			sorter: true,
-			filterable: true,
-			render: (text: string, record: any) =>
-				text ? moment(text).format("LL") : "",
-			renderFilter: ({ column, confirm, ref }: FilterParam) => (
-				<FilterDatePicker column={column} confirm={confirm} ref={ref} />
-			),
-		},
-		{
-			title: t("pages:documentsTemplate.table.action"),
-			key: "action",
-			render: (text, record) => {
-				return (
-					<Space size="middle">
-						<span
-							onClick={() => generatePdf(record)}
-							title="Download PDF"
-							style={{ cursor: "pointer" }}
-						>
-							<FilePdfOutlined style={{ fontSize: "21px" }} />
-						</span>
-						<span
-							title="Download CSV"
-							style={{ cursor: "pointer" }}
-						>
-							<FileOutlined style={{ fontSize: "20px" }} />
-						</span>
-					</Space>
-				);
-			},
-		},
-	];
+  const columns = [
+    {
+      title: t("pages:documents.table.formName"),
+      dataIndex: "formName",
+      key: "documents.formName",
+      sorter: true,
+      filterable: true,
+      render: (text, record) => {
+        return <strong>{text}</strong>;
+      },
+    },
+    {
+      title: t("pages:documentsTemplate.table.formId"),
+      dataIndex: "formId",
+      key: "documents.formId",
+      sorter: true,
+      filterable: true,
+    },
+    {
+      title: t("pages:documentsTemplate.table.issuedBy"),
+      dataIndex: "issuedBy",
+      key: "documents.issuedBy",
+      sorter: true,
+      filterable: true,
+    },
+    {
+      title: t("pages:documentsTemplate.table.issuedDate"),
+      dataIndex: "issuedDate",
+      key: "documents.issuedDate",
+      sorter: true,
+      filterable: true,
+      render: (text: string, record: any) =>
+        text ? moment(text).format("LL") : "",
+      renderFilter: ({ column, confirm, ref }: FilterParam) => (
+        <FilterDatePicker column={column} confirm={confirm} ref={ref} />
+      ),
+    },
+    {
+      title: t("pages:documentsTemplate.table.submitter"),
+      dataIndex: "submitter",
+      key: "documents.submitter",
+      sorter: true,
+      filterable: true,
+    },
+    {
+      title: t("pages:documentsTemplate.table.submitDate"),
+      dataIndex: "submitDate",
+      key: "documents.submitDate",
+      sorter: true,
+      filterable: true,
+      render: (text: string, record: any) =>
+        text ? moment(text).format("LL") : "",
+      renderFilter: ({ column, confirm, ref }: FilterParam) => (
+        <FilterDatePicker column={column} confirm={confirm} ref={ref} />
+      ),
+    },
+    {
+      title: t("pages:documentsTemplate.table.status"),
+      dataIndex: "status",
+      key: "documents.status",
+      sorter: true,
+      filterable: true,
+      render: (text, record) => {
+        return (
+          <div style={checkStatusColor(text)}>{checkStatusByName(text)}</div>
+        );
+      },
+    },
+    {
+      title: t("pages:documents.table.updatedDate"),
+      dataIndex: "updatedDate",
+      key: "documents.updatedDate",
+      sorter: true,
+      filterable: true,
+      render: (text: string, record: any) =>
+        text ? moment(text).format("LL") : "",
+      renderFilter: ({ column, confirm, ref }: FilterParam) => (
+        <FilterDatePicker column={column} confirm={confirm} ref={ref} />
+      ),
+    },
+    {
+      title: t("pages:documentsTemplate.table.action"),
+      key: "action",
+      render: (text, record) => {
+        return (
+          <Space size="middle">
+            <span
+              onClick={(e) => {
+                e.stopPropagation();
+                generatePdf(record);
+              }}
+              title="Download PDF"
+              style={{ cursor: "pointer" }}
+            >
+              <FilePdfOutlined style={{ fontSize: "21px" }} />
+            </span>
+            <span title="Download CSV" style={{ cursor: "pointer" }}>
+              <FileOutlined style={{ fontSize: "20px" }} />
+            </span>
+          </Space>
+        );
+      },
+    },
+  ];
 
-	return (
-		<>
-			<div className="content">
-				<div style={{ float: "right", marginBottom: "10px" }}>
-					{statusCount && (
-						<div>
-							<ButtonGroup>
-								<Button
-									onClick={() =>
-										console.log("All button is clicked")
-									}
-								>
-									<Badge
-										count={statusCount["Approve"]}
-										style={{
-											color: "#17B169",
-											backgroundColor: "#cefad0",
-										}}
-									/>
-									<span style={{ marginLeft: 5 }}>
-										{" "}
-										Approve
-									</span>
-								</Button>
-								<Button
-									onClick={() =>
-										console.log(
-											"To Be Reviewed button is clicked"
-										)
-									}
-								>
-									<Badge
-										count={statusCount["To Be Reviewed"]}
-										style={{
-											color: "#DAA520",
-											backgroundColor: "#FFF8DC",
-										}}
-									/>
-									<span style={{ marginLeft: 5 }}>
-										{" "}
-										To Be Reviewed
-									</span>
-								</Button>
-								<Button
-									onClick={() =>
-										console.log(
-											"Rejected button is clicked"
-										)
-									}
-								>
-									<Badge
-										count={statusCount["Rejected"]}
-										style={{
-											color: "#b22222",
-											backgroundColor: "#fff6f6",
-										}}
-									/>
-									<span style={{ marginLeft: 5 }}>
-										{" "}
-										Rejected
-									</span>
-								</Button>
-							</ButtonGroup>
-						</div>
-					)}
-				</div>
+  return (
+    <>
+      <div className="content">
+        <div style={{ float: "right", marginBottom: "10px" }}>
+          {statusCount && (
+            <div>
+              <ButtonGroup>
+                <Button onClick={() => console.log("All button is clicked")}>
+                  <Badge
+                    count={statusCount["Approve"]}
+                    style={{
+                      color: "#17B169",
+                      backgroundColor: "#cefad0",
+                    }}
+                  />
+                  <span style={{ marginLeft: 5 }}> Approve</span>
+                </Button>
+                <Button
+                  onClick={() =>
+                    console.log("To Be Reviewed button is clicked")
+                  }
+                >
+                  <Badge
+                    count={statusCount["To Be Reviewed"]}
+                    style={{
+                      color: "#DAA520",
+                      backgroundColor: "#FFF8DC",
+                    }}
+                  />
+                  <span style={{ marginLeft: 5 }}> To Be Reviewed</span>
+                </Button>
+                <Button
+                  onClick={() => console.log("Rejected button is clicked")}
+                >
+                  <Badge
+                    count={statusCount["Rejected"]}
+                    style={{
+                      color: "#b22222",
+                      backgroundColor: "#fff6f6",
+                    }}
+                  />
+                  <span style={{ marginLeft: 5 }}> Rejected</span>
+                </Button>
+              </ButtonGroup>
+            </div>
+          )}
+        </div>
 
-				<GridTable
-					ref={tableRef}
-					columns={columns}
-					fetchData={fetchData}
-					rowSelection={{
-						selectedRowKeys: selectedIds,
-						onChange: (data: any[]) => onChangeSelection(data),
-						...rowSelection,
-					}}
-					addIndexCol={false}
-					selectableRowsHighlight
-					onRow={(record, rowIndex) => {
-						// return {
-						// 	onClick: (event) => {
-						// 		redirect("frontend.admin.application.edit", { id: record.id });
-						// 	},
-						// };
-					}}
-				/>
-			</div>
-		</>
-	);
+        <GridTable
+          ref={tableRef}
+          columns={columns}
+          fetchData={fetchData}
+          rowSelection={{
+            selectedRowKeys: selectedIds,
+            onChange: (data: any[]) => onChangeSelection(data),
+            ...rowSelection,
+          }}
+          addIndexCol={false}
+          selectableRowsHighlight
+          onRow={(record, rowIndex) => {
+            return {
+              onClick: (event) => {
+                redirect("frontend.admin.application.edit", { id: record.id });
+              },
+            };
+          }}
+        />
+      </div>
+    </>
+  );
 };
 
 Index.Layout = (props) => {
-	const { t } = useBaseHook();
-	return (
-		<>
-			<Layout
-				title={t("pages:documentsTemplate.index.title")}
-				description={t("pages:documentsTemplate.index.description")}
-				{...props}
-			/>
-		</>
-	);
+  const { t } = useBaseHook();
+  return (
+    <>
+      <Layout
+        title={t("pages:documentsTemplate.index.title")}
+        description={t("pages:documentsTemplate.index.description")}
+        {...props}
+      />
+    </>
+  );
 };
 
 Index.permissions = {
-	document_templates: "R",
+  document_templates: "R",
 };
 
 export default Index;
